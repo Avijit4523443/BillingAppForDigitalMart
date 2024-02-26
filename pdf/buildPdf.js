caf074f8727ebfcdc2dd8e400aefb96c1a85099d
meavijit00011 <meavijitmaitywork@gmail.com>
2024-02-26 20:35:30 +0530

    first commit

---
+++ pdf/buildPdf.js
@@ -0,0 +1,29 @@
+const ejs = require('ejs');
+let pdf = require('html-pdf');
+const fs = require("fs");
+const path = require('path')
+const createPDF = async (req, res) => {
+  try {
+    let htmlString = fs.readFileSync(path.join(__dirname, '/invoice.ejs')).toString();
+
+    let ejsData = ejs.render(htmlString, req.data);
+
+    pdf.create(ejsData,
+    ).toStream((err,stream) => {
+      console.log(err);
+      stream.pipe(res);
+    })
+
+  }
+  catch (err) {
+    console.log(err);
+    if (res.saleAddedToDatabase == true) {
+      return res.status(206).json({ status: true, msg: "Sale Added But Failed to generate PDF. Please try again later !" })
+    }
+    res.status(500).json({ status: false, msg: 'Server Failed !' })
+  }
+
+
+}
+
+module.exports = createPDF;
