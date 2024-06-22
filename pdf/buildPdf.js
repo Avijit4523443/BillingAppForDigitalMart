0c6abd722dd9b047cdf97d1e50dadc2f6e290a60
Avijit4523443 <amaity09657@gmail.com>
2024-06-22 22:17:07 +0530

    Update buildPdf.js

--- pdf/buildPdf.js
+++ pdf/buildPdf.js
@@ -8,10 +8,18 @@ const createPDF = async (req, res) => {
 
     let ejsData = ejs.render(htmlString, req.data);
 
-    pdf.create(ejsData,{"phantomPath": "./node_modules/phantomjs-prebuilt/bin/phantomjs"}).toStream((err,stream) => {
-      console.log(err);
-      stream.pipe(res);
-    })
+    // pdf.create(ejsData,{"phantomPath": "./node_modules/phantomjs-prebuilt/bin/phantomjs"}).toStream((err,stream) => {
+    //   console.log(err);
+    //   stream.pipe(res);
+    // })
+      const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(ejsData);
+
+    const pdf = await page.pdf({ format: 'A4' });
+    await browser.close()
+    res.set({"Content-Type":"application/pdf"});
+    res.send(pdf);
 
   }
   catch (err) {
