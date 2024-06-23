78545199d2a8f593b3d0f66cd1d47b3dbff06fa0
Avijit4523443 <amaity09657@gmail.com>
2024-06-23 07:03:45 +0530

    Update buildPdf.js

--- pdf/buildPdf.js
+++ pdf/buildPdf.js
@@ -1,25 +1,40 @@
 const ejs = require('ejs');
-let pdf = require('html-pdf');
 const fs = require("fs");
 const path = require('path');
-const puppeteer = require("puppeteer");
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
+
 const createPDF = async (req, res) => {
   try {
     let htmlString = fs.readFileSync(path.join(__dirname, '/invoice.ejs')).toString();
-
     let ejsData = ejs.render(htmlString, req.data);
 
-    // pdf.create(ejsData,{"phantomPath": "./node_modules/phantomjs-prebuilt/bin/phantomjs"}).toStream((err,stream) => {
-    //   console.log(err);
-    //   stream.pipe(res);
-    // })
-      const browser = await puppeteer.launch();
+    let options = {};
+
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+      options = {
+        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      };
+    }
+
+    const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
     await page.setContent(ejsData);
 
     const pdf = await page.pdf({ format: 'A4' });
     await browser.close()
-    res.set({"Content-Type":"application/pdf"});
+    res.set({ "Content-Type": "application/pdf" });
     res.send(pdf);
 
   }
