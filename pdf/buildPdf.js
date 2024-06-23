2abf8fb74cb3b0364239359a339b2ef35382e66f
Avijit4523443 <amaity09657@gmail.com>
2024-06-23 08:57:42 +0530

    Update buildPdf.js

--- pdf/buildPdf.js
+++ pdf/buildPdf.js
@@ -5,30 +5,12 @@ const chromium = require("@sparticuz/chromium");
 const puppeteer = require("puppeteer-core");
 const stealthPlugin = require("puppeteer-extra-plugin-stealth");
 const puppeteerExtra = require("puppeteer-extra");
+const chromium = require("chrome-aws-lambda");
 const createPDF = async (req, res) => {
   try {
     let htmlString = fs.readFileSync(path.join(__dirname, '/invoice.ejs')).toString();
     let ejsData = ejs.render(htmlString, req.data);
-    require("puppeteer-extra-plugin-stealth/evasions/defaultArgs");
-    require("puppeteer-extra-plugin-user-preferences");
-    require("puppeteer-extra-plugin-user-data-dir");
-    require('puppeteer-extra-plugin-stealth/evasions/chrome.app');
-require('puppeteer-extra-plugin-stealth/evasions/chrome.csi');
-require('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes');
-require('puppeteer-extra-plugin-stealth/evasions/chrome.runtime');
-require('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow');
-require('puppeteer-extra-plugin-stealth/evasions/media.codecs');
-require('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency');
-require('puppeteer-extra-plugin-stealth/evasions/navigator.languages');
-require('puppeteer-extra-plugin-stealth/evasions/navigator.permissions');
-require('puppeteer-extra-plugin-stealth/evasions/navigator.plugins');
-require('puppeteer-extra-plugin-stealth/evasions/navigator.vendor');
-require('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver');
-require('puppeteer-extra-plugin-stealth/evasions/sourceurl');
-require('puppeteer-extra-plugin-stealth/evasions/user-agent-override');
-require('puppeteer-extra-plugin-stealth/evasions/webgl.vendor');
-require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions');
-    puppeteerExtra.use(stealthPlugin())
+   
 
   const options = {
       args: chromium.args,
@@ -39,7 +21,7 @@ require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions');
       ignoreDefaultArgs: ['--disable-extensions']
     }
   
-    const browser = await puppeteerExtra.launch(options);
+    const browser = await chromium.launch(options);
     const page = await browser.newPage();
     await page.setContent(ejsData);
 
